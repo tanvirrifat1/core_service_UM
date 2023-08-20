b110d09fa7031b8bacac46b51b35cda989d03b39
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-08-20 20:00:57 +0600

    added more

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,6 +1,7 @@
 import { AcademicSemester } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.service';
 
@@ -22,23 +23,16 @@ const insertIntoDb = async (
   }
 };
 
-const getAllSemester = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const result = await AcademicSemesterService.getAllSemester();
-    sendResponse<AcademicSemester>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'academic-semester created successfully!',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const getAllSemester = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.getAllSemester();
+
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'academic-semester created successfully!',
+    data: result,
+  });
+});
 
 export const AcademicSemesterController = {
   insertIntoDb,
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { academicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllSemester);
-router.post('/create-semester', AcademicSemesterController.insertIntoDb);
+router.post(
+  '/create-semester',
+  validateRequest(academicSemesterValidation.createSchema),
+  AcademicSemesterController.insertIntoDb
+);
 
 export const AcademicSemesterRouter = router;
