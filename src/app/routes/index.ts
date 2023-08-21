5a2e5967f23d174f423af89b94122e1baeefd78b
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-08-21 21:29:03 +0600

    created academicDepartment

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 import { AcademicSemesterRouter } from '../modules/academicSemester/academicSemester.route';
 
 const router = express.Router();
@@ -8,6 +9,10 @@ const moduleRoutes = [
   {
     path: '/academic-semester',
     route: AcademicSemesterRouter,
+  },
+  {
+    path: '/academic-department',
+    route: academicDepartmentRoutes,
   },
 ];
 
