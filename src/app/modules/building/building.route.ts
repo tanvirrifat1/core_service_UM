1e26fb371d03efebcbe99416785bc59d23db64a6
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-08-23 14:37:28 +0600

    added validation

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
+import { BuildingValidation } from './building.validation';
 
 const router = express.Router();
 
-router.post('/create-building', BuildingController.insertIntoDB);
+router.post(
+  '/create-building',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(BuildingValidation.create),
+  BuildingController.insertIntoDB
+);
 router.get('/', BuildingController.getAllFromDb);
 
 export const BuildingRoutes = router;
