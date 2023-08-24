4d0557c2288c5d8022edb50afe240e48d06a61f4
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-08-24 14:51:13 +0600

    added more

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { CourseController } from './course.controller';
 import { CourseValidation } from './course.validation';
@@ -8,16 +10,22 @@ const router = express.Router();
 router.get('/', CourseController.getAllFromDB);
 router.get('/:id', CourseController.getSingleDataFromDb);
 
-router.delete('/:id', CourseController.deleteData);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  CourseController.deleteData
+);
 
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(CourseValidation.update),
   CourseController.updatedData
 );
 
 router.post(
   '/create-course',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(CourseValidation.create),
   CourseController.insertIntoDb
 );
