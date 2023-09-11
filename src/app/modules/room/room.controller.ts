ba1d2fdcdf73450994835e935d24f2804b8d87d2
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-11 21:13:59 +0600

    added rooms information

--- src/app/modules/room/room.controller.ts
+++ src/app/modules/room/room.controller.ts
@@ -2,7 +2,9 @@ import { Room } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { roomFilterableFields } from './room.contants';
 import { RoomService } from './room.service';
 
 const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
@@ -16,6 +18,56 @@ const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, roomFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await RoomService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Rooms fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await RoomService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Room fetched successfully',
+    data: result,
+  });
+});
+
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await RoomService.updateOneInDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Room updated successfully',
+    data: result,
+  });
+});
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await RoomService.deleteByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Room deleted successfully',
+    data: result,
+  });
+});
+
 export const RoomController = {
   insertIntoDb,
+  getAllFromDB,
+  getByIdFromDB,
+  updateOneInDB,
+  deleteByIdFromDB,
 };
--- src/app/modules/room/room.route.ts
+++ src/app/modules/room/room.route.ts
@@ -1,8 +1,31 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { RoomController } from './room.controller';
+import { RoomValidation } from './room.validation';
 
 const router = express.Router();
 
-router.post('/room-created', RoomController.insertIntoDb);
+router.get('/', RoomController.getAllFromDB);
+router.get('/:id', RoomController.getByIdFromDB);
+
+router.post(
+  '/create',
+  validateRequest(RoomValidation.create),
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.insertIntoDb
+);
+
+router.patch(
+  '/:id',
+  validateRequest(RoomValidation.update),
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.deleteByIdFromDB
+);
 
 export const RoomRouters = router;
